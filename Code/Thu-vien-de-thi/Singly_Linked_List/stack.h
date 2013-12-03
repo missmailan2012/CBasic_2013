9659590240a3944c6166cd7640d56c96969b6afd
WinuxSke <WinuxSke@gmail.com>
2013-12-04 03:58:00 +0700

    Xong stack

--- Code/Thu-vien-de-thi/Singly_Linked_List/stack.h
+++ Code/Thu-vien-de-thi/Singly_Linked_List/stack.h
@@ -5,15 +5,18 @@ typedef struct
   TSLinkedList memberCore;
 }TStack;
 
+TSLinkedList coreOfStack(TStack stack);
+TSLinkedList* pCoreOfStack(TStack stack);
+
+boolean stackIsEmpty(TStack stack);
+boolean stackNotEmpty(TStack stack);
 boolean containerIsSyncStack(void* pContainer, TStack stack);
 boolean containerNotSyncStack(void* pContainer, TStack stack);
 
 void newStack(int nSizeOfData, TStack* pStack);
 void makeEmptyStack(TStack* pStack);
 void pushStack(void* pContainer, TStack* pStack);
-void* popStack(void* pNodeOut, TStack* pStack);
-void* viewTopStack(void* pNodeView, TStack stack);
-boolean stackIsEmpty(TStack stack);
-boolean stackNotEmpty(TStack stack);
-TSLinkedList coreOfStack(TStack stack);
-TSLinkedList* pCoreOfStack(TStack stack);
+void* popStack(void* pContainerOut, TStack* pStack);
+void viewTopStack(void* pContainerView, TStack stack);
+
+
---
+++ Code/Thu-vien-de-thi/Singly_Linked_List/stack_temp.c
@@ -0,0 +1,27 @@
+#include "stack.h"
+
+int main()
+{
+  TStack stack;
+  char count;
+  char* ch;
+
+  newStack(sizeof(char), &stack);
+  void* pContainerTemp;
+  pContainerTemp = newFullContainer(sizeof(char));
+  ch = (char*)addBlocksDataOfContainer(pContainerTemp);
+  for(count = 0; count < 6; count++)
+    {
+      MSG("Ban hay nhap vao mot so: ");
+      scanf("%c", ch);
+      while( getchar() != '\n');
+      pushStack(pContainerTemp, &stack);
+    }
+  for(count = 0; count < 6; count++)
+    {
+      popStack(pContainerTemp, &stack);
+      printf("%c----\n", *ch);
+    }
+  getchar();
+  return 0;
+}
