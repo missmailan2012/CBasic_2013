bf2c077e945eec6abd01eb952d846c3e3be5826d
WinuxSke <WinuxSke@gmail.com>
2013-12-04 15:14:34 +0700

    Ve nha roi

--- Code/Thu-vien-de-thi/Singly_Linked_List/nhap.c
+++ Code/Thu-vien-de-thi/Singly_Linked_List/nhap.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 
+void my_msg(char*);
+
 int main()
 {
   char s1[100];
   char s2[100];
-  printf("Nhap s1: ");
+  my_msg("Nhap s1: ");
   gets(s1);
   printf("Nhap s2: ");
   gets(s2);
   printf("s1: %s\ns2: %s\nstrcmp: %d\n", s1, s2, strcmp(s1, s2));
   getchar();
   return 0;
+}
+
+void my_msg(char* str)
+{
+  printf("%s\n", str);
+  return;
 }
